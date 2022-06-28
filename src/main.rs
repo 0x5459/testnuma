b983e4b7d74ff6fc09e91a82e6348de55fb44735
0x5459 <0x5459@protonmail.com>
2022-06-28 11:46:11 +0800

    fortest

--- src/main.rs
+++ src/main.rs
@@ -14,15 +14,15 @@ fn main() {
 
     if arg == 1 {
         let mut m = mmap::allocate_layer(sector_size).unwrap();
-        unsafe {
-            std::ptr::write_bytes::<u8>(m.as_mut_ptr(), 0, sector_size);
-        }
+        // unsafe {
+        //     std::ptr::write_bytes::<u8>(m.as_mut_ptr(), 0, sector_size);
+        // }
     } else {
         let mut m = numa::allocate_layer(sector_size);
-        let s = m.as_mut();
-        unsafe {
-            std::ptr::write_bytes::<u8>(s.as_mut_ptr(), 0, s.len());
-        }
+        // let s = m.as_mut();
+        // unsafe {
+        //     std::ptr::write_bytes::<u8>(s.as_mut_ptr(), 0, s.len());
+        // }
     };
 
     println!("elapsed: {}", now.elapsed().as_millis());
